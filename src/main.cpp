62ed277ce1092c366999d13149b5acfc3509a658
CARLOS\carlo <carlosmatecki2@gmail.com>
2025-01-13 16:28:14 -0500

    fixed resource handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,12 @@
 ﻿//
 
 
-
 // Created by carlo on 2024-09-21.
 //
 
 
-
-
-
-
-
-
 double deltaTime;
 double previousTime;
-
 
 
 #include "WindowAPI/WindowInclude.hpp"
@@ -34,7 +26,7 @@ CONST int WINDOWS_HEIGHT = 1024;
 void run(WindowProvider* windowProvider)
 {
     int imageCount = 3;
-    
+
     ENGINE::WindowDesc windowDesc = {};
     windowDesc.hInstance = GetModuleHandle(NULL);
     windowDesc.hWnd = glfwGetWin32Window(windowProvider->window);
@@ -49,44 +41,32 @@ void run(WindowProvider* windowProvider)
 
     std::unique_ptr<ENGINE::Core> core = std::make_unique<ENGINE::Core>(
         glfwExtensions, glfwExtensionCount, &windowDesc, enableDebugging);
- 
-    
-    
+
     std::unique_ptr<ENGINE::RenderGraph> renderGraph = core->CreateRenderGraph();
-    
+
     std::unique_ptr<ENGINE::InFlightQueue> inFlightQueue = std::make_unique<ENGINE::InFlightQueue>(
-        core.get(),renderGraph.get(), windowDesc, imageCount, vk::PresentModeKHR::eMailbox,
+        core.get(), renderGraph.get(), windowDesc, imageCount, vk::PresentModeKHR::eMailbox,
         windowProvider->GetWindowSize());
-    
+
     ENGINE::ResourcesManager* resourcesManager = ENGINE::ResourcesManager::GetInstance(core.get());
     std::unique_ptr<ENGINE::DescriptorAllocator> descriptorAllocator = std::make_unique<ENGINE::DescriptorAllocator>();
 
-
-
     Rendering::RenderingResManager* renderingResManager = Rendering::RenderingResManager::GetInstance();
     // Rendering::ModelLoader::GetInstance(core.get());
-    
-
-    std::vector<ENGINE::DescriptorAllocator::PoolSizeRatio> poolSizeRatios ={
-        {vk::DescriptorType::eSampler, 1.5f},
-        {vk::DescriptorType::eStorageBuffer, 1.5f},
-        {vk::DescriptorType::eUniformBuffer, 1.5f},
-        {vk::DescriptorType::eStorageImage, 1.5f},
-    };
-    descriptorAllocator->BeginPool(core->logicalDevice.get(), 10, poolSizeRatios);
 
     std::map<std::string, std::unique_ptr<Rendering::BaseRenderer>> renderers;
-    
+
     // renderers.try_emplace("ClusterRenderer", std::make_unique<Rendering::ClusterRenderer>(
-        // core.get(), windowProvider, descriptorAllocator.get()));
-     renderers.try_emplace("FlatRenderer", std::make_unique<Rendering::FlatRenderer>(core.get(), windowProvider));
+    // core.get(), windowProvider));
     
     // Rendering::ClusterRenderer* clusterRenderer = dynamic_cast<Rendering::ClusterRenderer*>(renderers.at("ClusterRenderer").get());
     // clusterRenderer->SetRenderOperation(inFlightQueue.get());
-   
+    
+    renderers.try_emplace("FlatRenderer", std::make_unique<Rendering::FlatRenderer>(core.get(), windowProvider));
+
     Rendering::FlatRenderer* flatRenderer = dynamic_cast<Rendering::FlatRenderer*>(renderers.at("FlatRenderer").get());
     flatRenderer->SetRenderOperation(inFlightQueue.get());
-    
+
     std::unique_ptr<Rendering::ImguiRenderer> imguiRenderer = std::make_unique<Rendering::ImguiRenderer>(
         core.get(), windowProvider, renderers);
 
@@ -101,9 +81,9 @@ void run(WindowProvider* windowProvider)
         float time = windowProvider->GetTime();
         deltaTime = time - previousTime;
         previousTime = time;
-        auto profiler =ENGINE::Profiler::GetInstance();
+        auto profiler = ENGINE::Profiler::GetInstance();
         profiler->StartProfiler();
-        
+
         windowProvider->PollEvents();
         {
             glm::uvec2 windowSize = windowProvider->GetWindowSize();
@@ -113,12 +93,12 @@ void run(WindowProvider* windowProvider)
                 core->WaitIdle();
                 inFlightQueue.reset();
                 inFlightQueue = std::make_unique<ENGINE::InFlightQueue>(
-                    core.get(), renderGraph.get(),windowDesc, imageCount, vk::PresentModeKHR::eMailbox,
+                    core.get(), renderGraph.get(), windowDesc, imageCount, vk::PresentModeKHR::eMailbox,
                     windowSize);
                 windowProvider->framebufferResized = false;
                 core->resizeRequested = false;
                 renderGraph->RecreateFrameResources();
-                for (auto& renderer :renderers) 
+                for (auto& renderer : renderers)
                 {
                     renderer.second->SetRenderOperation(inFlightQueue.get());
                 }
@@ -126,13 +106,14 @@ void run(WindowProvider* windowProvider)
             }
             try
             {
-                profiler->AddProfilerCpuSpot(legit::Colors::sunFlower,"Cpu");
+                profiler->AddProfilerCpuSpot(legit::Colors::sunFlower, "Cpu");
                 if (glfwGetKey(windowProvider->window, GLFW_KEY_R))
                 {
                     // clusterRenderer->ReloadShaders();
                     renderGraph->RecompileShaders();
                 }
-                if (glfwGetKey(windowProvider->window, GLFW_KEY_LEFT_CONTROL) && glfwGetKey(windowProvider->window, GLFW_KEY_R))
+                if (glfwGetKey(windowProvider->window, GLFW_KEY_LEFT_CONTROL) && glfwGetKey(
+                    windowProvider->window, GLFW_KEY_R))
                 {
                     renderGraph->DebugShadersCompilation();
                 }
@@ -140,27 +121,26 @@ void run(WindowProvider* windowProvider)
                 renderingResManager->UpdateResources();
                 resourcesManager->UpdateBuffers();
                 resourcesManager->UpdateImages();
-                
+
                 inFlightQueue->BeginFrame();
 
                 auto& currFrame = inFlightQueue->frameResources[inFlightQueue->frameIndex];
 
                 profiler->EndProfilerCpuSpot("Cpu");
-                
+
                 core->renderGraphRef->ExecuteAll(&currFrame);
 
 
-                profiler->AddProfilerCpuSpot(legit::Colors::alizarin,"Imgui");
+                profiler->AddProfilerCpuSpot(legit::Colors::alizarin, "Imgui");
                 imguiRenderer->RenderFrame(currFrame.commandBuffer.get(),
                                            inFlightQueue->currentSwapchainImageView->imageView.get());
 
                 profiler->EndProfilerCpuSpot("Imgui");
-                profiler->AddProfilerGpuSpot(legit::Colors::carrot,"Gpu");
+                profiler->AddProfilerGpuSpot(legit::Colors::carrot, "Gpu");
 
                 resourcesManager->EndFrameDynamicUpdates(currFrame.commandBuffer.get());
                 inFlightQueue->EndFrame();
                 profiler->EndProfilerGpuSpot("Gpu");
-
             }
             catch (vk::OutOfDateKHRError err)
             {
@@ -172,7 +152,6 @@ void run(WindowProvider* windowProvider)
         core->WaitIdle();
         profiler->EndProfilerGpuSpot("WaitIdle");
         profiler->UpdateProfiler();
-        
     }
     imguiRenderer->Destroy();
     ENGINE::ResourcesManager::GetInstance()->DestroyResources();
@@ -181,9 +160,10 @@ void run(WindowProvider* windowProvider)
 
 int main()
 {
-    std::unique_ptr<WindowProvider> windowProvider = std::make_unique<WindowProvider>(WINDOWS_WIDTH, WINDOWS_HEIGHT, "Vulkan Engine Template");
+    std::unique_ptr<WindowProvider> windowProvider = std::make_unique<WindowProvider>(
+        WINDOWS_WIDTH, WINDOWS_HEIGHT, "Vulkan Engine Template");
     windowProvider->InitGlfw();
-    
+
     run(windowProvider.get());
 
     windowProvider->Terminate();
